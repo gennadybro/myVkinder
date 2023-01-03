17cbf68bc70d64175c48eb25c12dfde4698afe58
gennadybro <gennady@gbronnikov.ru>
2023-01-03 15:44:25 +0300

    Работаю над выводом информации о пользователе

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+
 from random import randrange
 
 import vk_api
 import requests
 from vk_api.longpoll import VkLongPoll, VkEventType
-
-from config import my_token, group_token
+from urllib.parse import urlparse, urlencode
+from my_token import token, group_token
 # from keyboards import keyboard
 
 vk = vk_api.VkApi(token=group_token)  #Авторизация группы
@@ -20,9 +21,7 @@ def write_msg(user_id, message):  #  Отправка сообщений
 
 def uname(user_id):   #  имя пользователя
     url = f'https://api.vk.com/method/users.get'
-    params = {'access_token': my_token,
-              'user_ids': user_id,
-              'v': '5.131'}
+    params = {'user_ids': user_id, 'fields': 'bdate,city', 'access_token' : token, 'v': '5.131'}
     repl = requests.get(url, params=params)
     response = repl.json()
     print(response)
@@ -31,7 +30,11 @@ def uname(user_id):   #  имя пользователя
         for i in information_dict:
             for key, value in i.items():
                 first_name = i.get('first_name')
-                return first_name
+                status = i.get('is_closed')
+                bdate = i.get('bdate')
+                city = i.get('city')
+                print(bdate)
+                return  status, first_name, bdate, city
     except KeyError:
         write_msg(user_id, 'Ошибка получения токена, введите токен в переменную - my_token')
 
@@ -48,13 +51,22 @@ for event in longpoll.listen():
         user_id = str(event.user_id)
         msg = event.text.lower()
         sender(user_id, msg.lower())
-        if request == "начинаем":
-            write_msg(user_id, f"Привет, {uname((user_id))}")
+        if request == "start":
+            status, first_name, bdate, city = uname(user_id)
+            write_msg(user_id, f"Привет, {first_name}")   #{uname((user_id)
+            if status == True:
+                write_msg(user_id, "Ваша страница закрыта")
+            else:
+                write_msg(user_id, f"Ваша дата рождения,{bdate}")
+                if city == 'none':
+                    write_msg(user_id, "В вашем пофиле не указан город,заполните профиль")
+                else:
+                    get_city = city["title"]
+                    write_msg(user_id, f"Ваш город ,{get_city}")  # не верно выдает
         elif request == "вперёд":
             pass
-        elif request == "стоп":
-            write_msg(event.user_id, f"Досвидания, {uname(user_id)}")
+        elif request == "stop":
+            write_msg(event.user_id, f"Досвидания, {first_name}")
             break
         else:
             write_msg(event.user_id, "Твоё сообщение непонятно")
-
