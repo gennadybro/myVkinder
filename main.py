91b7f2388fefc6e8aade1bbe4a32063e143fd7b6
gennadybro <gennady@gbronnikov.ru>
2023-01-07 13:28:21 +0300

    Добавлен вызов функции записи результатов users.get

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import json
 from random import randrange
 from vk_api.longpoll import VkLongPoll, VkEventType
 from urllib.parse import urlparse, urlencode
-from my_token import token, group_token, token_foto
+from config import token, group_token, token
+from database import add_my_data
 # from vksearch import  user_search, write_msg
 # from keyboards import keyboard
 
@@ -27,6 +28,8 @@ class Vkbot:
     vk = vk_api.VkApi(token=group_token)  #Авторизация группы
     longpoll = VkLongPoll(vk)
     vk_me = vk_api.VkApi(token=token, api_version='5.131').get_api()
+    LIST_USERS = []
+
 
 def write_msg(user_id, message):  #  Отправка сообщений
     Vkbot.vk.method('messages.send', {
@@ -38,7 +41,7 @@ def write_msg(user_id, message):  #  Отправка сообщений
 
 def username(user_id):   #  сбор информации о  пользователе
     url = f'https://api.vk.com/method/users.get'
-    params = {'user_ids': user_id, 'fields': 'sex,bdate,city,relation', 'access_token' : token_foto, 'v': '5.131'}
+    params = {'user_ids': user_id, 'fields': 'sex,bdate,city,relation', 'access_token' : token, 'v': '5.131'}
     repl = requests.get(url, params=params)
     response = repl.json()
     try:
@@ -46,12 +49,15 @@ def username(user_id):   #  сбор информации о  пользоват
         for i in information_dict:
             for key, value in i.items():
                 first_name = i.get('first_name')
+                last_name = i.get('last_name')
                 relation = i.get('relation')
                 wall_relation = i.get('is_closed')
                 bdate = i.get('bdate')
                 city = i.get('city')
                 user_sex = i.get('sex')
-                print(first_name, relation)
+                date_year = int(bdate[-4:])
+                print(user_id, first_name, last_name, date_year, city["title"], user_sex, relation)
+                add_my_data(user_id, first_name, last_name, date_year, city["title"], user_sex, relation)
                 return  relation, first_name, bdate, city, user_sex, wall_relation
     except KeyError:
         write_msg(user_id, 'Ошибка получения токена, введите токен в переменную - my_token')
@@ -60,7 +66,7 @@ def username(user_id):   #  сбор информации о  пользоват
 def user_search(SEX, BDATE, CITY, RELATION):
     print(SEX," : ", BDATE," : ",CITY," : ",RELATION)
     url = f'https://api.vk.com/method/users.search'
-    params = {'access_token': token_foto,
+    params = {'access_token': token,
             'v': '5.131',
             'sex': 1, # if SEX == '1' else 2,
             'birth_year': BDATE,
@@ -70,11 +76,11 @@ def user_search(SEX, BDATE, CITY, RELATION):
             'count': 500}
     resp = requests.get(url, params=params)
     resp_json = resp.json()
-    print(resp_json)
+    # print(resp_json)
     try:
         dict_1 = resp_json['response']
         list_1 = dict_1['items']
-        filename = "Test"
+        # filename = "Test"
         for person_dict in list_1:
             if person_dict.get('is_closed') == False:
                 first_name = person_dict.get('first_name')
@@ -82,23 +88,37 @@ def user_search(SEX, BDATE, CITY, RELATION):
                 vk_id = str(person_dict.get('id'))
                 vk_link = 'vk.com/id' + str(person_dict.get('id'))
                 
-                #                   блок временный
-                if os.path.exists(f"{filename}"):
-                    print(f"Директория существует")
-                else:
-                    os.mkdir(filename)
-                with open(f"{filename}/{filename}.json", "w", encoding="utf-8") as file:
-                    json.dump(resp_json, file, indent=4, ensure_ascii=False)
-
-                #  
-                print(first_name," ",last_name," ",vk_id," ",vk_link)                    
-                return first_name, last_name, vk_id, vk_link
+                Vkbot.LIST_USERS.append({first_name, last_name, vk_id, vk_link}) 
             else:
                 continue
+            
+        #    блок временный
+            # resul = Vkbot.LIST_USERS.json()
+            # if os.path.exists(f"{filename}"):
+            #     print(f"Директория существует")
+            # else:
+            #     os.mkdir(filename)
+            # with open(f"{filename}/{filename}.json", "w", encoding="utf-8") as file:
+            #     json.dump(resul, file, indent=4, ensure_ascii=False)  #  resp_json    временно, для проверки записи в файл
+
+        #  
+            # print(first_name," ",last_name," ",vk_id," ",vk_link)                    
+        print(Vkbot.LIST_USERS)
         return f'Поиск завершён'
         
     except KeyError:
         write_msg(Vkbot.USER_ID, 'Ошибка получения токена')
+
+
+
+
+# def message_result_find(user_id):
+#     Vkbot.vk.method('messages.send', 
+#             {'user_id': user_id,
+#             'access_token': token_foto,
+#             'message': message,
+#             'attachment': f'photo{self.person_id(offset)}_{self.get_photo_3(self.person_id(offset))}',
+#             'random_id': 0})
 
 
 def get_sex(sex_user):
@@ -137,59 +157,60 @@ def get_foto_users(users_id):
         write_msg(users_id, 'Ошибка получения токена')
 
 
-    def get_photo_1(self, user_id):
-        """ПОЛУЧЕНИЕ ID ФОТОГРАФИИ № 1"""
-        list = self.get_photos_id(user_id)
-        count = 0
-        for i in list:
-            count += 1
-            if count == 1:
-                return i[1]
+def get_photo_1(users_id):
+    """ПОЛУЧЕНИЕ ID ФОТОГРАФИИ № 1"""
+    list = get_foto_users(users_id)
+    count = 0
+    for i in list:
+        count += 1
+        if count == 1:
+            return i[1]
 
 
-    def get_photo_2(self, user_id):
-        """ПОЛУЧЕНИЕ ID ФОТОГРАФИИ № 2"""
-        list = self.get_photos_id(user_id)
-        count = 0
-        for i in list:
-            count += 1
-            if count == 2:
-                return i[1]
+def get_photo_2(users_id):
+    """ПОЛУЧЕНИЕ ID ФОТОГРАФИИ № 2"""
+    list = get_foto_users(users_id)
+    count = 0
+    for i in list:
+        count += 1
+        if count == 2:
+            return i[1]
 
-    def get_photo_3(self, user_id):
-        """ПОЛУЧЕНИЕ ID ФОТОГРАФИИ № 3"""
-        list = self.get_photos_id(user_id)
-        count = 0
-        for i in list:
-            count += 1
-            if count == 3:
-                return i[1]
-
-
-    def send_photo_1(self, user_id, message, offset):
-        """ОТПРАВКА ПЕРВОЙ ФОТОГРАФИИ"""
-        self.vk.method('messages.send', {'user_id': user_id,
-                                         'access_token': token_foto,
-                                         'message': message,
-                                         'attachment': f'photo{self.person_id(offset)}_{self.get_photo_1(self.person_id(offset))}',
-                                         'random_id': 0})
+def get_photo_3(users_id):
+    """ПОЛУЧЕНИЕ ID ФОТОГРАФИИ № 3"""
+    list = get_foto_users(users_id)
+    count = 0
+    for i in list:
+        count += 1
+        if count == 3:
+            return i[1]
 
 
-    def send_photo_2(self, user_id, message, offset):
-        """ОТПРАВКА ВТОРОЙ ФОТОГРАФИИ"""
-        self.vk.method('messages.send', {'user_id': user_id,
-                                         'access_token': token_foto,
-                                         'message': message,
-                                         'attachment': f'photo{self.person_id(offset)}_{self.get_photo_2(self.person_id(offset))}',
-                                         'random_id': 0})
+def send_photo_1(user_id, message, offset):
+    """ОТПРАВКА ПЕРВОЙ ФОТОГРАФИИ"""
+    Vkbot.vk.method('messages.send', {'user_id': user_id,
+                                        'access_token': token_foto,
+                                        'message': message,
+                                        'attachment': f'photo{person_id(offset)}_{get_photo_1(person_id(offset))}',
+                                        'random_id': 0})
 
-    def send_photo_3(self, user_id, message, offset):
-        """ОТПРАВКА ТРЕТЬЕЙ ФОТОГРАФИИ"""
-        self.vk.method('messages.send', {'user_id': user_id,
-                                         'access_token': token_foto,
-                                         'message': message,
-                                         'attachment': f'photo{self.person_id(offset)}_{self.get_photo_3(self.person_id(offset))}',
-                                         'random_id': 0})
+
+def send_photo_2(self, user_id, message, offset):
+    """ОТПРАВКА ВТОРОЙ ФОТОГРАФИИ"""
+    Vkbot.vk.method('messages.send', {'user_id': user_id,
+                                        'access_token': token_foto,
+                                        'message': message,
+                                        'attachment': f'photo{self.person_id(offset)}_{self.get_photo_2(self.person_id(offset))}',
+                                        'random_id': 0})
+
+
+def send_photo_3(self, user_id, message, offset):
+    """ОТПРАВКА ТРЕТЬЕЙ ФОТОГРАФИИ"""
+    Vkbot.vk.method('messages.send', {'user_id': user_id,
+                                        'access_token': token_foto,
+                                        'message': message,
+                                        'attachment': f'photo{self.person_id(offset)}_{self.get_photo_3(self.person_id(offset))}',
+                                        'random_id': 0})
 
 
 for event in Vkbot.longpoll.listen():
@@ -199,8 +220,9 @@ for event in Vkbot.longpoll.listen():
         msg = event.text.lower()
         Vkbot.RELATION, myname, bdate, Vkbot.CITY, Vkbot.SEX, wall_relation = username(user_id)
         Vkbot.USER_ID = user_id
+        offset = 0
         if request == "привет":
-            write_msg(user_id, f"Здравствуйте, {myname}, добро пожаловать в бот \n  для поиска наберите start.")       
+            write_msg(user_id, f"Здравствуйте, {myname}, добро пожаловать в бот \n  для поиска наберите: start")       
         elif request == "start":
             if wall_relation == True:
                 write_msg(user_id, "Ваша страница закрыта")
@@ -213,15 +235,24 @@ for event in Vkbot.longpoll.listen():
                     break
                 else:
                     if Vkbot.SEX == '1':
-                        get_sex = "женшина"
+                        sex,find_sex = "мужчину",2
                     else:
-                        get_sex = "мужчина"
+                        sex,find_sex = "женщину",1
                     city_id, city = Vkbot.CITY["id"], Vkbot.CITY["title"]
-                    write_msg(user_id, f"Ваш город ,{city}, Вы {get_sex}") 
+                    write_msg(user_id, f"Ищем в г.,{city}, {sex}") 
+                    print(Vkbot.LIST_USERS)
+                    #first_name, last_name, vk_id, vk_link =  
+                    user_search(Vkbot.SEX,Vkbot.BDATE,city_id,Vkbot.RELATION)
+                    # foto = get_foto_users(vk_id)
+                    # write_msg(user_id, f"Вот что нашлось:  {first_name}, {last_name}, {vk_link}")    
+                    # send_photo_1(vk_id, "фото", offset)
         elif request == "next":
-            first_name, last_name, vk_id, vk_link =  user_search(Vkbot.SEX,Vkbot.BDATE,city_id,Vkbot.RELATION)
-            foto = get_foto_users(vk_id)
-            write_msg(user_id, f"Вот что нашлось:  {first_name} ,{last_name} , {vk_id} , {vk_link}, {foto}")    
+            offset += 1
+            # first_name, last_name, vk_id, vk_link =  user_search(Vkbot.SEX,Vkbot.BDATE,city_id,Vkbot.RELATION)
+            # foto = get_foto_users(vk_id)
+            # write_msg(user_id, f"Вот что нашлось:  {first_name} ,{last_name} , {vk_id} , {vk_link}, {foto}")    
+            # send_photo_1(user_id, "Первое фото", offset)
+
         elif request == "stop":
             write_msg(event.user_id, f"До свидания, {myname}")
             break
