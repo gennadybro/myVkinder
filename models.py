0983077dbd9ea5219027523c8781ff34044c75e2
gennadybro <gennady@gbronnikov.ru>
2023-01-08 20:57:31 +0300

    Внесены изменения

--- models.py
+++ models.py
@@ -3,18 +3,19 @@ from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
-class Users(Base):
-    __tablename__ = "users"
+class Partners(Base):
+    __tablename__ = "partners"
 
     id = sq.Column(sq.Integer, primary_key = True)
-    id_vk= sq.Column(sq.Integer, unique=True)
+    id_vk= sq.Column(sq.Integer, unique = True)
     first_name = sq.Column(sq.String())
     last_name = sq.Column(sq.String())
-    bdate = sq.Column(sq.Integer)
-    city = sq.Column(sq.String())
-    user_sex = sq.Column(sq.Integer)
-    relation = sq.Column(sq.Integer)
+    # bdate = sq.Column(sq.Integer)
+    # city = sq.Column(sq.String())
+    # user_sex = sq.Column(sq.Integer)
+    # relation = sq.Column(sq.Integer)
     pages = sq.Column(sq.String())
+    user_id = sq.Column(sq.Integer)
 
     # contents = relationship("content", back_populates="users")
 
@@ -23,19 +24,19 @@ class Content(Base):
     __tablename__  = "content"
 
     id_content = sq.Column(sq.Integer, primary_key = True)
-    id_user = sq.Column(sq.Integer, sq.ForeignKey("users.id"),nullable=False)
+    id_user = sq.Column(sq.Integer, sq.ForeignKey("partners.id"),nullable=False)
     url_foto1 = sq.Column(sq.String())
     url_foto2 = sq.Column(sq.String())
     url_foto3 = sq.Column(sq.String())
 
     # users = relationship("user", back_populates="content")
-    users = relationship(Users, backref="contents")
+    users = relationship(Partners, backref="contents")
 
-class my_data(Base):
-    __tablename__ = "my"
+class User(Base):
+    __tablename__ = "user"
 
     id = sq.Column(sq.Integer, primary_key = True)
-    id_vk= sq.Column(sq.Integer, unique=True)
+    id_vk= sq.Column(sq.Integer, unique = True)
     first_name = sq.Column(sq.String())
     last_name = sq.Column(sq.String())
     bdate = sq.Column(sq.Integer)
@@ -43,9 +44,18 @@ class my_data(Base):
     user_sex = sq.Column(sq.Integer)
     relation = sq.Column(sq.Integer)
 
+# class User_Partners(Base):
+#     __tablename__ = "user_parthers"
+#     user_id = sq.Column(sq.Integer, References=users(User.id)) 
+#     partners_id
+    def __str__(self):
+        return f'{self.id}, ({self.id_vk},{self.first_name},{self.last_name})'
 
 def create_tables(engine):
-    Base.metadata.drop_all(engine)   ###  Убрать !
+    # Base.metadata.drop_all(engine)   ###  Убрать !
     Base.metadata.create_all(engine)
 
+def drop_tables(engine):
+    Base.metadata.drop.Parners(engine)
 
+    
